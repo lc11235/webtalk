9ce0a267b4b4b69e23eae172f837fcb4384549c1
authurli <lc11235@qq.com>
2016-06-06 15:10:22 +0800

    bug fix

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -2,7 +2,7 @@ var Chat = function(socket){
     this.socket = socket;
 };
 
-Chat.prototype.sendMessage = function(room, text){
+Chat.prototype.sendMessage = function(room, text){//发送聊天消息的函数
     var message = {
         room: room,
         text: text
@@ -10,13 +10,13 @@ Chat.prototype.sendMessage = function(room, text){
     this.socket.emit('message', message);  
 };
 
-Chat.prototype.changeRoom = function(room){
+Chat.prototype.changeRoom = function(room){//变更房间的函数
     this.socket.emit('join', {
         newRoom: room
     });  
 };
 
-Chat.prototype.processCommand = function(command){
+Chat.prototype.processCommand = function(command){//处理聊天指令，包括join用来加入或创建一个房间，nick用来修改昵称
     var words = command.split(' ');
     var command = words[0]
                     .substring(1,words[0].length)
--- public/javascripts/chat_ui.js
+++ public/javascripts/chat_ui.js
@@ -1,8 +1,8 @@
-function divEscapedContentElement(message){
+function divEscapedContentElement(message){//处理用户输入，不可信，转义所有输入的字符
     return $('<div></div>').text(message);
 }
 
-function divSystemContentElement(message){
+function divSystemContentElement(message){//可信，直接插入到html代码中
     return $('<div></div>').html('<i>' + message + '</i>');
 }
 
@@ -10,13 +10,13 @@ function processUserInput(chatApp, socket){
     var message = $('#send-message').val();
     var systemMessage;
     
-    if(message.charAt(0) == '/'){
+    if(message.charAt(0) == '/'){//检查用户输入内容，以斜杠开头，将其作为聊天指令
         systemMessage = chatApp.processCommand(message);
         if(systemMessage){
             $('#messages').append(divSystemContentElement(systemMessage));
         }
     } else {
-        chatApp.sendMessage($('#room').text(), message);
+        chatApp.sendMessage($('#room').text(), message);//非命令输入广播给其他用户
         $('#messages').append(divEscapedContentElement(message));
         s('#messages').scrollTop($('#messages').prop('scrollHeight'));
     }
@@ -29,7 +29,7 @@ var socket = io.connect();
 $(document).ready(function(){
     var chatApp = new Chat(socket);
     
-    socket.on('nameResult', function(result){
+    socket.on('nameResult', function(result){//显示更名尝试的结果
         var message;
         
         if(result.success){
@@ -41,17 +41,17 @@ $(document).ready(function(){
         $('#messages').append(divSystemContentElement(message));
     });
     
-    socket.on('joinResult', function(result){
+    socket.on('joinResult', function(result){//显示房间变更结果
         $('#room').text(result.room);
         $('#messages').append(divSystemContentElement('Room changed.'));
     });
     
-    socket.on('message', function(message){
+    socket.on('message', function(message){//显示收到的消息
         var newElement = $('<div></div>').text(message.text);
         $('#messages').append(newElement);
     });
     
-    socket.on('rooms', function(rooms){
+    socket.on('rooms', function(rooms){//显示可用房间列表
         $('#room-list').empty();
         
         for(var room in rooms){
@@ -61,17 +61,19 @@ $(document).ready(function(){
             }
         }
         
-        $('#room-list div').click(function(){
+        $('#room-list div').click(function(){//点击房间名可以换到那个房间中
             chatApp.processCommand('/join ' + $(this).text());
             $('#send-messages').focus();
         });
     });
     
-    setInterval(function(){
+    setInterval(function(){//定期请求可用房间列表
         socket.emit('rooms');
     }, 1000);
     
-    $('#send-form').submit(function(){
+    $('#send-message').focus();
+    
+    $('#send-form').submit(function(){//提交表单可以发送聊天消息
         processUserInput(chatApp, socket);
         return false;
     }); 
